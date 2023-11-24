e7b33ffaa4c41030a4507fec1a583e8cb30706d4
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-24 13:32:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input =document.getElementById("fname");
+input.addEventListener("blur", onblur);
+function onblur() {
+	input.value= input.value.toUpperCase();
+	
+	
+}
